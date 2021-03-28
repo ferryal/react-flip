7490f7491f1d4070f48e8641ce9385508452471c
ferryal <ferryal.bahari@amartha.com>
2021-03-28 21:04:51 +0700

    feat: collect export components

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+// Main Components
+export { default as Layout } from './Layout';
+export { default as Card } from './Card';
+export { default as Button } from './Button';
+export { default as Loading } from './Loading';
+export { default as Navbar } from './Navbar';
+export { default as SearchBar } from './SearchBar';
+export { default as Filter } from './Filter';
